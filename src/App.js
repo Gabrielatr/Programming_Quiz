1a5a3f34d9c3fa4f3d52a1e10d0e14ab72b8a540
Victor <contactvictorsouza@gmail.com>
2024-01-05 21:17:11 +0000

    Login e cadastro

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import './App.css';
-import Home from "./pages/Home"
-import Quiz from "./pages/Quiz/Quiz"
-
+import { Login } from './pages/Login';
+import { Cadastro } from './pages/Cadastro';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
+    <Router>
+      <Routes>
+        <Route path="/login" exact element={<Login/>} />
+        <Route path="/cadastro" element={<Cadastro/>} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
+
 
 export default App;
